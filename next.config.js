371e962870270bdc32aa24a3b023873c18b0618d
Vu Hung <vuhung@mizzerx.com>
2022-07-20 06:55:51 +0700

    [Config] Templary disable images optimization

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: 'imgix',
-    path: '/',
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
   },
 };
 
